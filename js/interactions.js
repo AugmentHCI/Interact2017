157f7a4ab7feaa6274f607c954a1685013a8e6e8
robindecroon <robindecroon@msn.com>
2016-07-28 15:54:58 +0200

    interactions and warnings added

--- js/interactions.js
+++ js/interactions.js
@@ -18,15 +18,21 @@ svg.attr('width', width)
     .attr('height', height);
 
 var rectangleLayer = svg.append('svg');
-rectangleLayer.attr('width', width)
+rectangleLayer
+    .attr("class", "rectangleLayer")
+    .attr('width', width)
     .attr('height', height);
 
 var medicationLayer = svg.append('svg');
-medicationLayer.attr('width', width)
+medicationLayer
+    .attr("class", "medicationLayer")
+    .attr('width', width)
     .attr('height', height);
 
 var auxLayer = svg.append('svg');
-auxLayer.attr('width', width)
+auxLayer
+    .attr("class", "auxLayer")
+    .attr('width', width)
     .attr('height', height);
 
 function drawRectangle(data, titleText, startX, startY) {
@@ -67,20 +73,10 @@ function drawRectangle(data, titleText, startX, startY) {
     singleEpisodeGroup.append("text").text(d => d.name).style("text-anchor", "middle")
         .attr("x", rectangleWidth / 2)
         .attr("y", rectangleHeight / 2)
+        .attr("dy", 5)
         .attr("class", "rectangleText");
 }
 
-// /**
-//  * Create arc for the given medication box with radius, color start and end angle
-//  */
-// function createPosArc(medBox,radius,startAngle,endAngle,color) {
-//     d3.select("#" + medBox.name)
-//         .append("path")
-//         .attr("class", "med_arc")
-//         .style("fill", color)
-//         .attr("d", innerArc(radius, startAngle,endAngle)(medBox));
-// }
-//
 
 // Arc auxiliary functin
 function innerArc(innerRadius, startAngle, endAngle) {
@@ -98,6 +94,13 @@ var arc = d3.arc()
     .startAngle(0);
 
 
+function numberOfCircles(halfLife) {
+    var nbCircles = 1;
+    if (halfLife > 60) nbCircles++;
+    if (halfLife > 60 * 24) nbCircles++;
+    if (halfLife > 60 * 24 * 7) nbCircles++;
+    return nbCircles;
+}
 d3.json('data/janedoe.json', function (error, data) {
     var nbEpisodes = data.episodes.length;
     var episodeStartX = (width - rectangleWidth * nbEpisodes - (nbEpisodes - 1) * padding) / 2;
@@ -109,16 +112,67 @@ d3.json('data/janedoe.json', function (error, data) {
     var allergyStartY = height - rectangleHeight - 2 * padding;
     drawRectangle(data.allergies, "Allergies", allergyStartX, allergyStartY);
 
-
     var personalStartX = 2 * padding;
     var personalStartY = allergyStartY;
     var personalData = [{name: data.personal.name}, {name: data.personal.age}, {name: data.personal.gender}, {name: data.personal.weight}];
     drawRectangle(personalData, "Personal", personalStartX, personalStartY);
 
-    // setInterval(update, 100);
+    setInterval(update, 1000);
     update();
     function update() {
         d3.json('data/locations.json', function (error, locations) {
+            var tempInteractions = [];
+
+            locations.forEach(med1 => {
+               med1.interactions.forEach(interaction => {
+                   locations.forEach(med2 => {
+                       if(interaction.name === med2.name) {
+                           tempInteractions.push({from: med1, to: med2, type: interaction.type});
+                       }
+                   });
+               });
+            });
+
+            // filter double interactions
+            var interactions = [];
+            tempInteractions.forEach(temp => {
+                if (interactions.findIndex(elem => elem.from == temp.to && elem.to == temp.from) < 0) {
+                    interactions.push(temp);
+                }
+            });
+
+            var interactionLines = medicationLayer.selectAll("line").data(interactions);
+
+            interactionLines.exit()
+                .attr("class", "linesExit")
+              .transition(t)
+                .style("fill-opacity", 1e-6)
+                .remove();
+
+            interactionLines
+                .transition(t)
+                .attr("x1", d => d.from.center[0])
+                .attr("y1", d => d.from.center[1])
+                .attr("x2", d => d.to.center[0])
+                .attr("y2", d => d.to.center[1]);
+
+            interactionLines
+                .enter()
+                .append("line")
+                .attr("stroke-width", 5)
+                .attr("stroke", d => {
+                    if (d.type == "severe") {
+                        return "red";
+                    } else {
+                        return "orange";
+                    }
+                })
+                .attr("x1", d => d.from.center[0])
+                .attr("y1", d => d.from.center[1])
+                .attr("x2", d => d.to.center[0])
+                .attr("y2", d => d.to.center[1]);
+
+
             // JOIN new data with old elements.
             var myGroups = medicationLayer.selectAll('g.circleGroup').data(locations);
 
@@ -139,10 +193,42 @@ d3.json('data/janedoe.json', function (error, data) {
               .enter().append("g")
                 .attr("class", "circleGroup")
                 .attr("transform", d => "translate(" + d.center[0] + "," + d.center[1] + ")");
+            //
+            // var warningCircles = myGroupsEnter.append("circle")
+            //     .attr("class", "warning")
+            //     .attr("cx", d => -(d.radius + 3 * padding + 3* radiusWidth))
+            //     .attr("cy", d => -(d.radius + 3 * padding + 3* radiusWidth))
+            //     .attr("r", 30);
 
+
+            var warningImages = myGroupsEnter.selectAll(".warnings").data(d =>
+                _.map(d.warnings, elem => {return {warning: elem, parentData: d}})
+            )
+
+            var imageSize = 40;
+            warningImages
+              .enter().append("svg:image")
+                .attr('x',(d,i) => {
+                    var nbCircles = numberOfCircles(d.parentData.halfLife);
+                    return i*40 -imageSize -0.75 * (d.parentData.radius + nbCircles*radiusWidth + nbCircles*padding)
+                })
+                .attr('y',(d,i) => {
+                    var nbCircles = numberOfCircles(d.parentData.halfLife);
+                    return -i*20-imageSize -0.75 * (d.parentData.radius + nbCircles*radiusWidth + nbCircles*padding)
+                })
+                .attr('width', imageSize)
+                .attr('height', imageSize)
+                .attr("xlink:href",d => "images/warning/" + d.warning + ".png")
+
+            // var warningImages = myGroupsEnter.append("svg:image")
+            //     .attr('x',-9)
+            //     .attr('y',-12)
+            //     .attr('width', 50)
+            //     .attr('height', 50)
+            //     .attr("xlink:href",d => "images/warning/" + d.warnings[0] + ".png")
 
             var minutesG = myGroupsEnter.append("g");
-            minutesG.append("path")
+            var minutes = minutesG.append("path")
                 .attr("class", "minutesArc")
                 .attr("d", d => innerArc(
                     d.radius,
@@ -156,11 +242,12 @@ d3.json('data/janedoe.json', function (error, data) {
                 })
               .enter().append("path")
                 .attr("class", "minutesAux")
-                .attr("d", d => innerArc(
+                .attr("d", (d) => {return innerArc(
                     d.parentData.radius,
                     (Math.PI/30)* d.n,
                     (Math.PI/30)* (d.n+0.1))(d.n)
-                );
+                });
+
 
             var hoursG = myGroupsEnter.append("g");
             hoursG.append("path")
@@ -191,8 +278,8 @@ d3.json('data/janedoe.json', function (error, data) {
                 var all = _.map(d3.range(0,7), num => {return {n: num, parentData: d}});
                 var result =  _.filter(all, e => e.n < Math.floor((d.halfLife/60)/24));
                 return result;
-            })
-                .enter().append("path")
+                })
+              .enter().append("path")
                 .attr("class", "daysAux")
                 .attr("d", (d) => {return innerArc(
                     d.parentData.radius + 2*radiusWidth + 2*padding,
@@ -208,8 +295,8 @@ d3.json('data/janedoe.json', function (error, data) {
                 var all = _.map(d3.range(0,51), num => {return {n: num, parentData: d}});
                 var result =  _.filter(all, e => e.n < Math.floor(((d.halfLife/60)/24)/7));
                 return result;
-            })
-                .enter().append("path")
+                })
+              .enter().append("path")
                 .attr("class", "weeksAux")
                 .attr("d", (d) => {return innerArc(
                     d.parentData.radius + 3*radiusWidth + 3*padding,
